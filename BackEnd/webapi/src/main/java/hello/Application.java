9efa867d60b0a903bf1310d50aa0600492c22bff
rushidesai1 <rushidesai1@gmail.com>
2017-02-24 23:02:58 -0600

    Sample Rest application

---
+++ BackEnd/webapi/src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by desair4 on 2/24/2017.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(hello.Application.class, args);
+    }
+}
