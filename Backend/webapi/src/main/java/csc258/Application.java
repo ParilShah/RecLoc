364f33b1552ba594c2a38f65b333e82af543069e
rushidesai1 <rushidesai1@gmail.com>
2017-05-02 22:01:42 -0500

    Removing logic for creating data and inserting dummy data from bootstrap to data.sql
    data.sql will be run by bootstrap automatically

--- Backend/webapi/src/main/java/csc258/Application.java
+++ Backend/webapi/src/main/java/csc258/Application.java
@@ -1,7 +1,5 @@
 package csc258;
 
-import csc258.bootstrap.Bootstrap;
-import csc258.spring.ApplicationContextProvider;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,8 +15,8 @@ public class Application {
     @Bean
     public CommandLineRunner demo() {
         return (args) -> {
-            Bootstrap bootstrap = ApplicationContextProvider.context.getBean(Bootstrap.class);
-            bootstrap.init();
+//            Bootstrap bootstrap = ApplicationContextProvider.context.getBean(Bootstrap.class);
+//            bootstrap.init();
         };
     }
 }
